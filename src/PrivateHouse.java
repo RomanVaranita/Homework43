94452d868817469b3cd59b7d34974184e7f6991d
Roman Varanita <romanvaranita@gmail.com>
2023-03-19 13:04:26 +0100

    Add toString

--- src/PrivateHouse.java
+++ src/PrivateHouse.java
@@ -14,4 +14,9 @@ public class PrivateHouse extends House {
   public void setAddress(String address) {
     super.setAddress(address);
   }
+
+  @Override
+  public String toString() {
+    return super.getAddress();
+  }
 }
