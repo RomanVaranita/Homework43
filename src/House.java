fd51765657d0da8a0833543f2ea0cd368173813a
Roman Varanita <romanvaranita@gmail.com>
2023-03-19 12:19:38 +0100

    Add abstract class House

---
+++ src/House.java
@@ -0,0 +1,16 @@
+public abstract class House {
+  private String address;
+
+
+  public House(String address) {
+    this.address = address;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+}
